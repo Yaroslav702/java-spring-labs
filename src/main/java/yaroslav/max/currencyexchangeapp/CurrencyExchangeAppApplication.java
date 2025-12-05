29f4598feede0a583adb53a29bd3604768f7f119
vysotskyi <manypresents34@gmail.com>
2025-12-05 14:44:24 +0100

    Initial commit.

---
+++ src/main/java/yaroslav/max/currencyexchangeapp/CurrencyExchangeAppApplication.java
@@ -0,0 +1,13 @@
+package yaroslav.max.currencyexchangeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeAppApplication.class, args);
+    }
+
+}
